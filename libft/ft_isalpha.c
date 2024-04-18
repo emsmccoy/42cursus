4e693c8ff672d5112ff8ac0967b4e88ad9c263ac
emsmccoy <65463392+emsmccoy@users.noreply.github.com>
2024-04-18 18:29:50 +0200

    modified isalpha and created isdigit

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+
 int	ft_isalpha(int c)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1024);
+	else
+		return (0);
 }
 
-int	main(void)
+/*int	main(void)
 {
 	printf("%d\n", ft_isalpha('a'));
 	printf("%d\n", ft_isalpha('A'));
 	printf("%d\n", ft_isalpha('1'));
 	printf("%d\n", ft_isalpha('$'));
 	return (0);
-}
+}*/
