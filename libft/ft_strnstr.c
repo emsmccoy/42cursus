45d27f4684066578d8dd12c0780f7a17856c41e4
emsmccoy <65463392+emsmccoy@users.noreply.github.com>
2024-07-26 23:01:28 +0200

    fix strnstr

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -16,18 +16,22 @@
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
-	size_t	needle_len;
+	size_t	j;
 
-	needle_len = ft_strlen(needle);
-	if (needle_len == 0)
-		return ((char *)haystack);
-	if (len == 0 || needle_len > len)
-		return (NULL);
 	i = 0;
-	while (haystack[i] != '\0' && i <= (len - needle_len))
+	if (!haystack && len == 0)
+		return (0);
+	if (needle[i] == '\0' || !needle)
+		return ((char *)haystack);
+	while (haystack[i] && i < len)
 	{
-		if (ft_strncmp(haystack + i, needle, needle_len) == 0)
-			return ((char *)haystack + i);
+		j = 0;
+		while (haystack[i + j] == needle[j] && i + j < len)
+		{
+			j++;
+			if (needle[j] == '\0')
+				return ((char *) haystack + i);
+		}
 		i++;
 	}
 	return (NULL);
