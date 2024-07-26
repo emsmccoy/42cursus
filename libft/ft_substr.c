87fb1945c662c74d41a12e9b89fda5a3d463375c
emsmccoy <65463392+emsmccoy@users.noreply.github.com>
2024-07-26 16:20:34 +0200

    fix memcpy strchr substr

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -18,18 +18,20 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	s_len;
 	char	*substr;
+	size_t	cut_len;
 
 	if (!s)
 		return (NULL);
-	s_len = ft_strlen((char *)s);
-	if (start > s_len)
+	s_len = ft_strlen(s);
+	if (start >= s_len)
 		return (ft_strdup(""));
-	if (s_len - start >= len)
-		substr = (char *)malloc((len + 1) * sizeof(char));
+	if (s_len - start < len)
+		cut_len = s_len - start;
 	else
-		substr = (char *)malloc((len + 1) * sizeof(char));
+		cut_len = len;
+	substr = (char *)malloc((cut_len + 1) * sizeof(char));
 	if (!substr)
 		return (NULL);
-	ft_strlcpy(substr, (s + start), (len + 1));
+	ft_strlcpy(substr, (s + start), (cut_len + 1));
 	return (substr);
 }
